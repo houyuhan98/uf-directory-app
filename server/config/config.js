3a053779cbbd0a62443ec43fb246fd2792ae8511
houyuhan98 <46461019+houyuhan98@users.noreply.github.com>
2019-03-20 14:54:17 -0400

    Add files via upload

---
+++ server/config/config.js
@@ -0,0 +1,9 @@
+//This file holds any configuration variables we may need 
+//'config.js' is typically ignored by git to protect sensitive information, such as your database's username and password
+
+module.exports = {
+  db: {
+    uri: 'mongodb://bootcamp3:Blessyuhan98@ds153314.mlab.com:53314/ufapp', //place the URI of your mongo database here.
+  },
+  port: 8080
+};
